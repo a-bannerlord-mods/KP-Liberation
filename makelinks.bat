56d8e23ee65536b193c6e3a659e7ba1595f85ddc
Ahmed Salah <ahmed007boss@hotmail.com>
2021-12-20 12:12:57 +0200

    remove KPPLM

---
+++ makelinks.bat
@@ -0,0 +1,3 @@
+robocopy  ".\Missionframework" ".\Missionbasefiles\kp_liberation.lythium"   /XO  /s /e *.*
+robocopy  ".\Missionframework" ".\Missionbasefiles\kp_liberation.Takistan"   /XO  /s /e *.*
+robocopy  ".\Missionframework" ".\Missionbasefiles\kp_liberation.pja310"   /XO  /s /e *.*
